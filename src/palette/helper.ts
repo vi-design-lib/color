60794b04399e70265a09ff050138e4f541a62201
ZhuChongLin <8210856@qq.com>
2024-12-25 00:40:54 +0800

    refactor(palette): 重构调色板功能并增加支持返回 RGB 颜色对象
    - 重构了 getPaletteColor 和 makePalette 函数，增加了对 RGB颜色对象的支持
    - 更新了函数签名，使用泛型来支持不同的返回类型
    - 优化了代码结构，提高了可读性和可维护性
    - 移除了未使用的 PaletteOptions 类型导入

--- src/palette/helper.ts
+++ src/palette/helper.ts
@@ -1,28 +1,24 @@
-import {
-  colorToRgbObj,
-  type HEXColor,
-  type RgbColor,
-  type RgbColorObj,
-  rgbToHex,
-  rgbToString
-} from '../utils/index.js'
-import { Palette, type PaletteOptions } from './palette.js'
+// noinspection JSUnusedGlobalSymbols
+
+import { colorToRgbObj, rgbToHex, rgbToString } from '../utils/index.js'
+import { Palette } from './palette.js'
+import type { HexColor, Out, OutType, RgbColor, RGBColor } from '../types.js'
 
 /**
  * 获取调色板颜色
  *
  * @param {number} i - 色阶索引，必须小于色阶数量
- * @param {RgbColorObj} sourceColor - 源色
+ * @param {RGBColor} sourceColor - 源色
  * @param {number} steps - 色阶数量，建议单数，颜色过渡更平滑(中间值则是源色)
  * @param {'hex' | 'rgb'} type - 返回的颜色类型，默认为 HEX
- * @returns {string} - 调色板颜色
+ * @returns {HexColor | RgbColor | RGBColor} - 调色板颜色
  */
-export function getPaletteColor(
+export function getPaletteColor<OUT extends OutType = 'hex'>(
   i: number,
-  sourceColor: RgbColorObj,
+  sourceColor: RGBColor,
   steps: number,
-  type: 'hex' | 'rgb' = 'hex'
-): string {
+  type: OUT = 'hex' as OUT
+): Out<OUT> {
   if (i > steps) throw new Error('i must be less than steps')
   const { r, g, b } = sourceColor
   let newR, newG, newB
@@ -42,8 +38,9 @@ export function getPaletteColor(
     newG = Math.round(g * (1 - factor) + 255 * factor)
     newB = Math.round(b * (1 - factor) + 255 * factor)
   }
-  const newRgb: RgbColorObj = { r: newR, g: newG, b: newB }
-  return type === 'rgb' ? rgbToString(newRgb) : rgbToHex(newRgb)
+  const newRgb: RGBColor = { r: newR, g: newG, b: newB }
+  if (type === 'RGB') return newRgb as Out<OUT>
+  return (type === 'rgb' ? rgbToString(newRgb) : rgbToHex(newRgb)) as Out<OUT>
 }
 
 /**
@@ -53,35 +50,22 @@ export function getPaletteColor(
  * 1. 只是获取某个色阶的颜色，请使用 `getPaletteColor` 函数。
  * 2. `new Palette(sourceColor, steps)` 实例化{@link Palette}对象，然后使用 Palette 对象提供的方法来获取颜色性能更佳。
  *
- * @param {RgbColorObj} sourceColor - 传入的原始颜色
+ * @param {RgbColor|HexColor|RGBColor} sourceColor - 源颜色
  * @param {number} steps - 色阶的数量
  * @param {'hex' | 'rgb'} [type=hex] - 返回的颜色类型，默认为 HEX
- * @returns {string[]} - 调色板颜色数组(黑->源->白)
+ * @returns {Array<HexColor | RgbColor | RGBColor>} - 调色板颜色数组(黑->源->白)
  */
-export function makePaletteFromRgb(
-  sourceColor: RgbColorObj,
+export function makePalette<OUT extends OutType = 'hex'>(
+  sourceColor: RgbColor | HexColor | RGBColor,
   steps: number = 11,
-  type: 'hex' | 'rgb' = 'hex'
-): string[] {
-  const palette: string[] = []
-  for (let i = 0; i < steps; i++) {
-    palette.push(getPaletteColor(i, sourceColor, steps, type))
+  type: OutType = 'hex'
+): Array<Out<OUT>> {
+  if (typeof sourceColor === 'string') {
+    sourceColor = colorToRgbObj(sourceColor)
   }
-  return palette
-}
-
-/**
- * 创建调色板
- *
- * @param {RgbColor | HEXColor} sourceColor - 源色
- * @param {number} [steps=11] - 色阶数量
- * @param {'hex' | 'rgb'} [type=hex] - 返回的颜色类型，默认为 hex
- * @returns {string[]} - 调色板颜色数组(黑->源->白)
- */
-export function makePalette(
-  sourceColor: RgbColor | HEXColor,
-  steps: number = 11,
-  type: 'hex' | 'rgb' = 'hex'
-): string[] {
-  return makePaletteFromRgb(colorToRgbObj(sourceColor), steps, type)
+  const palette: Array<Out<OUT>> = []
+  for (let i = 0; i < steps; i++) {
+    palette.push(getPaletteColor(i, sourceColor, steps, type) as Out<OUT>)
+  }
+  return palette as Array<Out<OUT>>
 }
