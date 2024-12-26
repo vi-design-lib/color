a05674be6da2f7914cdae6222183b6d96817bdd0
ZhuChongLin <8210856@qq.com>
2024-12-26 17:16:01 +0800

    feat(types): 新增颜色类型和配色方案接口
    - 新增 StrColors 类型，支持多种字符串颜色表示方式
    - 定义 BaseColorScheme 接口，用于描述基准配色方案
    - 添加 ColorSchemePalette 类型，用于表示配色方案调色板
    - 引入 Palette 类型，以支持颜色调色板功能

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { Palette } from './palette/index.js'
+
 /**
  * 16进制颜色类型
  */
@@ -17,6 +19,11 @@ export type RgbaColor = `rgba(${number}, ${number}, ${number}, ${number})`
  * rgb颜色类型
  */
 export type RgbColor = `rgb(${number}, ${number}, ${number})`
+
+/**
+ * 字符串颜色类型
+ */
+export type StrColors = HexColor | HslColor | RgbColor | string
 
 /**
  * RGB颜色对象类型
@@ -50,3 +57,48 @@ export type Out<T extends OutType> = T extends 'hex'
     : T extends 'RGB'
       ? RGBObject
       : HSLObject
+
+/**
+ * 基准配色方案
+ */
+export interface BaseColorScheme<T> {
+  /**
+   * 主色
+   */
+  primary: T
+  /**
+   * 次要辅色
+   *
+   * 主色相邻的颜色，通过调整饱和度和亮度来形成对比度
+   */
+  secondary: T
+  /**
+   * 三级辅色
+   *
+   * 主色相邻的颜色，通过调整饱和度和亮度来形成对比度
+   */
+  tertiary: T
+  /**
+   * 警告色
+   *
+   * 默认是主色的互补色，形成强烈的对比度
+   */
+  warning: T
+  /**
+   * 危险色
+   *
+   * 默认是红色，根据主色调整了饱和度和亮度
+   */
+  danger: T
+  /**
+   * 中性色
+   *
+   * 根据主色计算出来的中性色，它接近于灰色，但它和主色之间存在一定的色相关联
+   */
+  neutral: T
+}
+
+/**
+ * 配色方案
+ */
+export type ColorSchemePalette<T extends RgbColor | HexColor> = BaseColorScheme<Palette<T>>
