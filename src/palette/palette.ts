7128b4b8e4117d296140d9edf17ec88e48c0a214
ZhuChongLin <8210856@qq.com>
2024-12-26 19:27:29 +0800

    refactor(palette): 重构调色板类
    - 更新导入的工具函数
    - 调整类模板参数和属性
    - 修改颜色处理逻辑
    - 优化类方法和迭代器实现

--- src/palette/palette.ts
+++ src/palette/palette.ts
@@ -1,45 +1,44 @@
-import { colorToRgbObj } from '../utils/index.js'
+import { anyColorToRgbObject, getColorType } from '../utils/index.js'
 import { getPaletteColor } from './helper.js'
-import type { HexColor, Out, OutType, RgbColor, RGBObject, StrColors } from '../types.js'
+import type { AnyColor, ColorTag, HexColor, RgbColor, RGBObject } from '../types.js'
 
 /**
  * 调色板类
  *
  * 该类实现了`Symbol.iterator`方法，支持迭代。
  *
- * @template S - 源色类型，可以是RGB字符串或者HEX字符串
+ * @template T - 源色类型
  */
-export class Palette<S extends StrColors, OUT extends OutType = 'hex'> {
+export class Palette<T extends AnyColor = AnyColor> {
   // 源色RGB对象
   readonly #sourceRgb: RGBObject
   // 缓存色阶颜色
-  readonly #cacheColors: Array<Out<OUT>> = []
+  readonly #cacheColors: Array<T> = []
   // 色阶数量
   readonly #size: number
-  // 色阶颜色类型
-  readonly #outType: OUT
   // 源色
-  readonly #sourceColor: S
+  readonly #sourceColor: T
+  // 输出类型
+  readonly #outType: ColorTag
   /**
    * @param {RgbColor | HexColor} sourceColor - 源色
    * @param {number} size - 色阶数量，建议单数，不能小于9
-   * @param {OutType} outType - 色阶颜色类型，可以是`hex`|`rgb`|`RGB`
    */
-  constructor(sourceColor: S, size: number, outType: OUT = 'hex' as OUT) {
+  constructor(sourceColor: T, size: number) {
     if (typeof size !== 'number' || size < 9) {
       throw new Error('steps must be a number and greater than 9')
     }
     this.#sourceColor = sourceColor
     this.#size = size
-    this.#outType = outType
-    this.#sourceRgb = colorToRgbObj(sourceColor)
+    this.#sourceRgb = anyColorToRgbObject(sourceColor)
     this.#cacheColors.length = size
+    this.#outType = getColorType(sourceColor)
   }
 
   /**
    * 源色
    */
-  get source(): S {
+  get source(): T {
     return this.#sourceColor
   }
 
@@ -51,44 +50,32 @@ export class Palette<S extends StrColors, OUT extends OutType = 'hex'> {
   }
 
   /**
-   * 输出色彩类型
-   */
-  get outType(): OUT {
-    return this.#outType
-  }
-
-  /**
    * 获取色阶颜色
    *
    * @param {number} i - 色阶索引，必须小于色阶数量
    */
-  get(i: number): Out<OUT> {
+  get(i: number): T {
     if (!this.#cacheColors[i]) {
-      this.#cacheColors[i] = getPaletteColor(
-        i,
-        this.#sourceRgb,
-        this.size,
-        this.outType
-      ) as Out<OUT>
+      this.#cacheColors[i] = getPaletteColor(i, this.#sourceRgb, this.size, this.#outType) as T
     }
     return this.#cacheColors[i]
   }
   /**
    * 获取所有色阶颜色
    */
-  all(): Array<Out<OUT>> {
-    return Array.from(this) as Array<Out<OUT>>
+  all(): Array<T> {
+    return Array.from(this) as Array<T>
   }
 
   /**
    * 低代器
    */
-  [Symbol.iterator](): { next(): IteratorResult<Out<OUT>> } {
+  [Symbol.iterator](): { next(): IteratorResult<T> } {
     let index = 0
     const length = this.size
     // 返回一个迭代器对象
     return {
-      next: (): IteratorResult<Out<OUT>> => {
+      next: (): IteratorResult<T> => {
         if (index < length) {
           return { value: this.get(index++), done: false }
         } else {
@@ -105,14 +92,9 @@ export class Palette<S extends StrColors, OUT extends OutType = 'hex'> {
    * @template O - 色阶颜色类型，可以是`hex`|`rgb`|`RGB`
    * @param {RgbColor | HexColor} sourceColor - 源色
    * @param {number} [steps=11] - 色阶数量
-   * @param outType
    * @returns {Palette<T>} - 调色板实例
    */
-  static create<T extends RgbColor | HexColor, O extends OutType = 'hex'>(
-    sourceColor: T,
-    steps: number = 11,
-    outType: O = 'hex' as O
-  ): Palette<T, O> {
-    return new Palette(sourceColor, steps, outType)
+  static create<T extends RgbColor | HexColor>(sourceColor: T, steps: number = 11): Palette<T> {
+    return new Palette(sourceColor, steps)
   }
 }
