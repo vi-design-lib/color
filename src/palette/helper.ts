c132e5bfcbb6b5f3e742d45b42a795fb225bb268
ZhuChongLin <8210856@qq.com>
2024-12-26 14:31:08 +0800

    refactor(palette): 调整颜色类型定义和函数参数
    - 将 RGBColor 类型更名为 RGBObject，以更准确地表示颜色对象
    - 更新相关函数的参数和返回值类型，以适应新的颜色类型
    - 优化代码可读性和一致性，为后续功能扩展做准备

--- src/palette/helper.ts
+++ src/palette/helper.ts
@@ -2,20 +2,20 @@
 
 import { colorToRgbObj, rgbToHex, rgbToString } from '../utils/index.js'
 import { Palette } from './palette.js'
-import type { HexColor, Out, OutType, RgbColor, RGBColor } from '../types.js'
+import type { HexColor, Out, OutType, RgbColor, RGBObject } from '../types.js'
 
 /**
  * 获取基于 RGB 模型的调色板颜色
  *
  * @param {number} i - 色阶索引，必须小于色阶数量
- * @param {RGBColor} sourceColor - 源色
+ * @param {RGBObject} sourceColor - 源色
  * @param {number} steps - 色阶数量，建议单数，颜色过渡更平滑(中间值则是源色)
  * @param {'hex' | 'rgb' | 'hsl'} type - 返回的颜色类型，默认为 HEX
- * @returns {HexColor | RgbColor | RGBColor} - 调色板颜色
+ * @returns {HexColor | RgbColor | RGBObject} - 调色板颜色
  */
 export function getPaletteColor<OUT extends OutType = 'hex'>(
   i: number,
-  sourceColor: RGBColor,
+  sourceColor: RGBObject,
   steps: number,
   type: OUT = 'hex' as OUT
 ): Out<OUT> {
@@ -38,7 +38,7 @@ export function getPaletteColor<OUT extends OutType = 'hex'>(
     newG = Math.round(g * (1 - factor) + 255 * factor)
     newB = Math.round(b * (1 - factor) + 255 * factor)
   }
-  const newRgb: RGBColor = { r: newR, g: newG, b: newB }
+  const newRgb: RGBObject = { r: newR, g: newG, b: newB }
   if (type === 'RGB') return newRgb as Out<OUT>
   return (type === 'rgb' ? rgbToString(newRgb) : rgbToHex(newRgb)) as Out<OUT>
 }
@@ -50,13 +50,13 @@ export function getPaletteColor<OUT extends OutType = 'hex'>(
  * 1. 只是获取某个色阶的颜色，请使用 `getPaletteColor` 函数。
  * 2. `new Palette(sourceColor, steps)` 实例化{@link Palette}对象，然后使用 Palette 对象提供的方法来获取颜色性能更佳。
  *
- * @param {RgbColor|HexColor|RGBColor} sourceColor - 源颜色
+ * @param {RgbColor|HexColor|RGBObject} sourceColor - 源颜色
  * @param {number} steps - 色阶的数量
  * @param {'hex' | 'rgb'} [type=hex] - 返回的颜色类型，默认为 HEX
- * @returns {Array<HexColor | RgbColor | RGBColor>} - 调色板颜色数组(黑->源->白)
+ * @returns {Array<HexColor | RgbColor | RGBObject>} - 调色板颜色数组(黑->源->白)
  */
 export function makePalette<OUT extends OutType = 'hex'>(
-  sourceColor: RgbColor | HexColor | RGBColor,
+  sourceColor: RgbColor | HexColor | RGBObject,
   steps: number = 11,
   type: OutType = 'hex'
 ): Array<Out<OUT>> {
