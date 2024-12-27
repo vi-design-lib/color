89e575b652c40bd9d0943c5934768b189db5301e
ZhuChongLin <8210856@qq.com>
2024-12-27 13:18:48 +0800

    refactor(scheme): 优化配色方案生成逻辑
    -简化代码结构，移除不必要的变量和注释
    - 优化辅色和三级辅色的生成逻辑，确保饱和度低于主色
    - 调整中性色的生成方式，增加主色调的占比
    - 保留两位小数，提高精度

--- src/scheme/helper.ts
+++ src/scheme/helper.ts
@@ -20,12 +20,10 @@ export function createBaseColorScheme<T extends StringColors | ObjectColors>(
   // 获取主色的 HSL 值
   const { h, s, l } = primaryHsl
 
-  // 降低饱和度，确保状态色不超过主色饱和度
-  const adjustedSaturation = HslFormula.ratioAdjust(s, 0.9)
-
   // 亮度调整，确保不会过亮或过暗
-  const adjustedLightness = Math.min(Math.max(l * 1.1, 0), 1) // 保持亮度在 [0, 1] 范围内
-  const sl = { s: adjustedSaturation, l: adjustedLightness }
+  const adjustedLightness = parseFloat(Math.min(Math.max(l * 1.1, 0), 1).toFixed(2)) // 保持亮度在 [0, 1] 范围内
+
+  const sl = { s: HslFormula.ratioAdjust(s, 0.9), l: adjustedLightness }
 
   // 使用主色的反向互补颜色做为警告颜色
   const warningHsl = { h: HslFormula.complementaryHue(h), ...sl }
@@ -34,19 +32,20 @@ export function createBaseColorScheme<T extends StringColors | ObjectColors>(
   const dangerHsl = { h: 0, ...sl } // 红色范围
 
   // 生成辅色和三级辅色的色相
-  const secondaryHue = HslFormula.adjacentHue(h, -30) // 基于主色的相邻色 反向偏移
+  const minorHue = HslFormula.adjacentHue(h, -30) // 基于主色的相邻色 反向偏移
   const tertiaryHue = HslFormula.adjacentHue(h, 30) // 基于主色的相邻色，正向偏移
 
   // 确保辅色和三级辅色的饱和度低于主色
-  const secondaryHsl = { h: secondaryHue, ...sl }
-  const tertiaryHsl = { h: tertiaryHue, ...sl }
+  const minorHsl = { h: minorHue, s: HslFormula.ratioAdjust(s, 0.8), l: adjustedLightness }
+  const tertiaryHsl = { h: tertiaryHue, s: HslFormula.ratioAdjust(s, 0.7), l: adjustedLightness }
 
   // 生成中性色：灰色调带有主色调
   const neutralHsl = { h, s: 0.1, l: l * 0.9 } // 灰色带有主色调的HSL
+
   // 创建 HSL 配色方案
   const hslScheme: ColorScheme<HSLObject> = {
     primary: primaryHsl,
-    secondary: secondaryHsl,
+    minor: minorHsl,
     tertiary: tertiaryHsl,
     warning: warningHsl,
     danger: dangerHsl,
