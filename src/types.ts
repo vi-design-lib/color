ca2d77384f23e67231388f4fbc2eb08b261081a8
ZhuChongLin <8210856@qq.com>
2024-12-27 22:45:57 +0800

    refactor(types): 重构颜色相关类型定义
    - 重命名 ColorScheme 为 BaseColorScheme，用于定义基准配色方案
    - 添加自定义颜色角色和中性色角色的类型定义
    - 定义配色方案调色板和颜色提取规则的类型
    - 优化类型定义，提高可扩展性和可维护性

--- src/types.ts
+++ src/types.ts
@@ -63,10 +63,13 @@ export type ColorTagToColorType<T extends ColorTag> = T extends 'hex'
         : T extends 'HSL'
           ? HSLObject
           : never
+
 /**
  * 基准配色方案
+ *
+ * 如有自定义配色方案，重写该接口，添加配色方案。
  */
-export interface ColorScheme<T extends AnyColor = AnyColor> {
+export interface BaseColorScheme<T extends AnyColor = AnyColor> {
   /**
    * 主色
    */
@@ -76,7 +79,7 @@ export interface ColorScheme<T extends AnyColor = AnyColor> {
    *
    * 主色相邻的颜色，通过调整饱和度和亮度来形成对比度
    */
-  minor: T
+  secondary: T
   /**
    * 三级辅色
    *
@@ -101,10 +104,134 @@ export interface ColorScheme<T extends AnyColor = AnyColor> {
    * 根据主色计算出来的中性色，它接近于灰色，但它和主色之间存在一定的色相关联
    */
   neutral: T
+
   /**
-   * 自定义配色方案
+   * 自定义颜色
    */
   [key: string]: T
 }
 
-export type ColorSchemePalettes<T extends ColorScheme> = Record<keyof T, Palette<T['primary']>>
+/**
+ * 配色方案调色板
+ */
+export type ColorSchemePalettes<T extends BaseColorScheme> = Record<keyof T, Palette<T['primary']>>
+
+/**
+ * 调色板提取颜色规则
+ */
+export type PaletteExtractionColorRules = {
+  /**
+   * 源色容器颜色
+   */
+  container: number
+  /**
+   * 源色容器之上的文本颜色
+   */
+  onContainer: number
+  /**
+   * 源色
+   */
+  source: number
+  /**
+   * 源色之上的文本颜色
+   */
+  onSource: number
+}
+
+/**
+ * 配色方案对应的颜色角色
+ *
+ * 借鉴于{@link https://material.io/design/color/the-color-system.html#color-roles Material Design}。
+ *
+ * `neutral` 调色板转化的角色较多，单独处理，具体见 {@link NeutralColorRoles} 接口
+ */
+export type ColorSchemeRoles<T extends BaseColorScheme> = Omit<T, 'neutral'> & {
+  // 排除 neutral 字段，并生成 "on" 开头的字段
+  [key in Exclude<keyof T, 'neutral'> as `on${Capitalize<key & string>}`]: T[key]
+} & {
+  // 排除 neutral 字段，并生成 "keyContainer" 的字段
+  [key in Exclude<keyof T, 'neutral'> as `${key & string}Container`]: T[key]
+} & {
+  // 排除 neutral 字段，并生成 "on{key}Container" 的字段
+  [key in Exclude<keyof T, 'neutral'> as `on${Capitalize<key & string>}Container`]: T[key]
+} & NeutralColorRoles<T['primary']>
+
+/**
+ * 中性调色板生成的角色
+ */
+export interface NeutralColorRoles<T extends AnyColor> {
+  /**
+   * 默认的背景颜色
+   */
+  surface: T
+
+  /**
+   * 反色表面容器颜色
+   */
+  inverseSurface: T
+
+  /**
+   * 反色表面容器颜色之上的文本颜色
+   */
+  inverseOnSurface: T
+
+  /**
+   * 表面最暗淡的颜色
+   */
+  surfaceDim: T
+
+  /**
+   * 最亮表面颜色
+   */
+  surfaceBright: T
+
+  /**
+   * 表面容器最低层的颜色
+   */
+  surfaceContainerLowest: T
+
+  /**
+   * 表面容器颜色
+   */
+  surfaceContainer: T
+
+  /**
+   * 低强调容器颜色
+   */
+  surfaceContainerLow: T
+
+  /**
+   * 高强调容器颜色
+   */
+  surfaceContainerHigh: T
+
+  /**
+   * 最高强调容器颜色
+   */
+  surfaceContainerHighest: T
+
+  /**
+   * 背景之上的文本颜色
+   */
+  onSurface: T
+
+  /**
+   * 背景之上的文本颜色，低强调浅灰色
+   */
+  onSurfaceVariant: T
+
+  /**
+   * 通常用于描边，中性色
+   */
+  outline: T
+
+  /**
+   * 通常用于分割线，偏向于灰色
+   */
+  outlineVariant: T
+
+  /**
+   * 阴影颜色，通常需要降低透明度使用
+   */
+  shadow: T
+}
