9f4dde906b895375e66a4cbe548f68fc1a9fd5ba
ZhuChongLin <8210856@qq.com>
2024-12-27 22:44:47 +0800

    feat(theme): 添加 createTheme 函数
    - 实现了 createTheme函数，用于创建主题实例
    - 支持自定义配色方案，可以覆盖基本配色方案
    - 添加了颜色类型检查，确保自定义颜色与主色类型一致
    - 优化了代码结构，提高了主题系统的可扩展性和易用性

---
+++ src/theme/helper.ts
@@ -0,0 +1,40 @@
+import type { AnyColor, BaseColorScheme } from '../types.js'
+import { getColorType } from '../utils/index.js'
+import { Theme } from './theme.js'
+import { Scheme } from './scheme.js'
+
+/**
+ * 创建主题
+ *
+ * @template T - 颜色类型
+ * @param {T} primary - 主色
+ * @param {Record<string,T>} customColorScheme - 自定义配色方案，如果和基本配色方案重名，会覆盖基本配色方案
+ * @returns {Theme} - 主题实例
+ */
+export function createTheme<T extends AnyColor, CustomColorScheme extends Record<string, T>>(
+  primary: T,
+  customColorScheme?: CustomColorScheme
+): Theme<BaseColorScheme<T> & CustomColorScheme> {
+  const primaryColorType = getColorType(primary)
+  // 创建基本配色方案
+  const colorsScheme = Scheme.createBaseColorScheme(primary)
+  // 合并自定义配色方案
+  if (typeof customColorScheme === 'object' && !Array.isArray(customColorScheme)) {
+    for (const colorsKey in customColorScheme) {
+      const value = customColorScheme[colorsKey]
+      let type
+      try {
+        type = getColorType(value)
+      } catch (e) {
+        throw new Error(`custom color scheme ${colorsKey} is not a valid color`)
+      }
+      if (type !== primaryColorType) {
+        throw new Error(
+          `custom color scheme ${colorsKey} color type must be the same as the primary color type`
+        )
+      }
+      colorsScheme[colorsKey] = value
+    }
+  }
+  return new Theme(colorsScheme as BaseColorScheme<T> & CustomColorScheme)
+}
