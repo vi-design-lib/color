fcf67f9dec36f11dbaf7b62b6b519049bc5c2b66
ZhuChongLin <8210856@qq.com>
2025-02-05 23:04:23 +0800

    feat(theme): 新增主题管理功能
    - 添加 Theme 类，实现主题模式切换、颜色方案变更、CSS 变量生成等功能
    - 在 src 目录下新增 theme 文件夹，包含主题相关代码
    - 更新 src/index.ts，引入新增的主题管理模块

---
+++ src/theme/theme.ts
@@ -0,0 +1,237 @@
+import type {
+  AnyColor,
+  ColorSchemeKeys,
+  ColorSchemeRoles,
+  ColorToColorType,
+  ExpandColorSchemeRoles,
+  TonalKeys,
+  Tone
+} from '../types.js'
+import { createScheme, Scheme } from '../scheme/index.js'
+import { anyColorToHexColor, camelToKebab } from '../utils/index.js'
+
+/**
+ * 亮度
+ */
+export type Bright = 'light' | 'dark'
+/**
+ * 主题模式
+ */
+export type ThemeMode = Bright | 'system'
+type RefFn = <T>(value: T) => { value: T }
+type Ref<T> = { value: T }
+const ref = <T>(value: T): Ref<T> => {
+  return { value }
+}
+export interface ThemeOptions {
+  /**
+   * 缓存主题模式的key
+   *
+   * @default '_CACHE_THEME_MODE'
+   */
+  cacheKey: string
+}
+/**
+ * 主题管理类
+ *
+ * 依赖浏览器端`CSSStyleSheet`和`matchMedia`特性，自动生成css变量样式表，支持动态切换主题。
+ */
+export class Theme<T extends AnyColor, CustomKeys extends string> {
+  /**
+   * ref代理函数
+   *
+   * 为了同时兼容`vitarx`和`vue`框架，提供此静态属性，用于指定`ref`函数来源，内部默认使用的是一个伪ref函数，
+   * 不具备响应式能力。
+   */
+  static refProxy: RefFn = ref
+  // 主题模式
+  private _mode: Ref<ThemeMode> = Theme.refProxy(this.getCacheThemeMode())
+  // 样式表
+  private readonly sheet: CSSStyleSheet
+  // 颜色方案
+  private _scheme: Ref<Scheme<ColorToColorType<T>>>
+  private options: ThemeOptions
+  constructor(
+    primary: T,
+    customColorScheme: Record<CustomKeys, ColorToColorType<T>>,
+    options?: Partial<ThemeOptions>
+  ) {
+    this.options = Object.assign(
+      {
+        cacheKey: '_CACHE_THEME_MODE'
+      },
+      options
+    )
+    this.sheet = this.createStyleSheet()
+    this._scheme = Theme.refProxy(createScheme(primary, customColorScheme))
+    this.updateStyles()
+  }
+  /**
+   * 创建一个样式表
+   *
+   * @returns {CSSStyleSheet} - CSSStyleSheet。
+   */
+  private createStyleSheet(): CSSStyleSheet {
+    let cssSheet: CSSStyleSheet
+    if ('CSSStyleSheet' in window && 'adoptedStyleSheets' in document) {
+      cssSheet = new CSSStyleSheet()
+      document.adoptedStyleSheets.push(cssSheet)
+    } else {
+      const style = document.createElement('style')
+      style.appendChild(
+        document.createComment('此样式表由@vi-design/color注入与管理，请勿外部变更。')
+      )
+      document.head.append(style)
+      cssSheet = style.sheet!
+    }
+    return cssSheet
+  }
+
+  /**
+   * 设置主题模式
+   *
+   * @param mode
+   * @protected
+   */
+  set mode(mode: ThemeMode) {
+    const oldBright = this.bright
+    this._mode.value = mode
+    if (this.bright === oldBright) return
+    // 缓存主题
+    localStorage.setItem('_CACHE_THEME_MODE', mode)
+    // 切换样式
+    document.body.setAttribute('data-theme', this.bright)
+    this.updateStyles()
+  }
+
+  /**
+   * 获取主题模式
+   */
+  get mode() {
+    return this._mode.value
+  }
+
+  /**
+   * 动态切换颜色方案
+   *
+   * @param primary
+   * @param customColorScheme
+   */
+  public changeColorScheme(
+    primary: ColorToColorType<T>,
+    customColorScheme?: Record<CustomKeys, ColorToColorType<T>>
+  ) {
+    this._scheme.value = createScheme(primary as unknown as T, customColorScheme)
+    this.updateStyles()
+  }
+
+  /**
+   * 获取当前主题的亮度
+   */
+  get bright(): Bright {
+    const mode = this.mode
+    return mode === 'system' ? Theme.systemBright : mode
+  }
+
+  /**
+   * 获取当前主题的颜色方案
+   */
+  get scheme(): Readonly<Scheme<ColorToColorType<T>>> {
+    return this._scheme.value
+  }
+
+  /**
+   * 获取角色颜色
+   *
+   * @param role
+   */
+  roles(role: keyof ColorSchemeRoles<T> | CustomKeys) {
+    return this.scheme[this.bright].roles[role as keyof ColorSchemeRoles<T>]
+  }
+
+  /**
+   * 获取色调颜色
+   *
+   * @param role
+   * @param tone
+   */
+  tonal(role: ColorSchemeKeys | CustomKeys, tone: Tone) {
+    if (this.bright === 'dark') {
+      return this.scheme.tonalPalettes[role as ColorSchemeKeys].all().reverse()[tone - 1]
+    } else {
+      return this.scheme.tonalPalettes[role as ColorSchemeKeys].get(tone - 1)
+    }
+  }
+
+  /**
+   * 获取css变量
+   *
+   * @param {} key
+   * @return {string} css变量
+   */
+  cssVar(
+    key:
+      | keyof ColorSchemeRoles<T>
+      | keyof ExpandColorSchemeRoles<T, CustomKeys>
+      | CustomKeys
+      | TonalKeys
+      | `${CustomKeys}-${Tone}`
+  ): `var(--color-${string})` {
+    return `var(--color-${camelToKebab(key)})`
+  }
+
+  /**
+   * 更新样式
+   *
+   * @private
+   */
+  protected updateStyles() {
+    while (this.sheet.cssRules.length > 0) {
+      this.sheet.deleteRule(0)
+    }
+    const generateRoleStyles = (scheme: Scheme<AnyColor>, theme: 'dark' | 'light') => {
+      return Object.keys(scheme[theme].roles)
+        .map((rule) => {
+          const color = scheme[theme].roles[rule as 'primary']
+          return `--color-${camelToKebab(rule)}: ${color};`
+        })
+        .join('\n')
+    }
+
+    const generateTonalStyles = (scheme: Scheme<AnyColor>, theme: 'dark' | 'light') => {
+      return Object.entries(scheme[theme].tonal)
+        .map(([key, value]) => {
+          return `--color-${camelToKebab(key)}: ${anyColorToHexColor(value)};`
+        })
+        .join('\n')
+    }
+
+    const lightStyles =
+      generateRoleStyles(this.scheme, 'light') + generateTonalStyles(this.scheme, 'light')
+    const darkStyles =
+      generateRoleStyles(this.scheme, 'dark') + generateTonalStyles(this.scheme, 'dark')
+    this.sheet.insertRule(`body[data-theme="light"]{${lightStyles}}`, 0)
+    this.sheet.insertRule(`body[data-theme="dark"]{${darkStyles}}`, 1)
+  }
+
+  /**
+   * 获取系统亮度
+   */
+  static get systemBright(): Bright {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  }
+
+  /**
+   * 获取缓存的主题
+   */
+  private getCacheThemeMode(): ThemeMode {
+    return (localStorage.getItem(this.options.cacheKey) || 'system') as ThemeMode
+  }
+
+  /**
+   * 清除缓存
+   */
+  public clearCache() {
+    localStorage.removeItem(this.options.cacheKey)
+  }
+}
