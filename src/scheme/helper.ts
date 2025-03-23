22e9d457b96cd1b78c26386cf48e1a53e11dd5b2
ZhuChongLin <8210856@qq.com>
2025-03-24 03:06:38 +0800

    feat(scheme): 优化 createScheme 函数参数设计
    - 新增 ColorSchemeOptions 接口，用于定义 createScheme 函数的可选参数
    - 在 ColorSchemeOptions 中添加 formula、angle 和 customColorScheme 属性
    - 修改 createScheme函数签名，使用新的 ColorSchemeOptions 类型
    - 更新函数内部逻辑，支持新的参数配置

--- src/scheme/helper.ts
+++ src/scheme/helper.ts
@@ -1,22 +1,41 @@
 import type { AnyColor, ColorScheme, ColorToColorType } from '../types.js'
-import { getColorType } from '../utils/index.js'
+import { type ComputeFormula, getColorType } from '../utils/index.js'
 import Scheme from './scheme.js'
 
+export interface ColorSchemeOptions<T extends AnyColor> {
+  /**
+   * 计算模式
+   *
+   * @default 'triadic'
+   */
+  formula?: ComputeFormula
+  /**
+   * 色相偏移角度
+   */
+  angle?: number
+  /**
+   * 自定义颜色方案
+   *
+   * 如果和基本配色方案重名，会覆盖基本配色方案
+   */
+  customColorScheme?: Record<string, T>
+}
 /**
  * 创建主题
  *
  * @template T - 颜色类型
  * @param {T} primary - 主色，支持16进制字符串颜色、rgb颜色、hsl颜色、颜色对象
- * @param {Record<string,T>} customColorScheme - 自定义配色方案，如果和基本配色方案重名，会覆盖基本配色方案
+ * @param options
  * @returns {Scheme} - 主题实例
  */
 export function createScheme<T extends AnyColor>(
   primary: T,
-  customColorScheme?: Record<string, ColorToColorType<T>>
+  options?: ColorSchemeOptions<ColorToColorType<T>>
 ): Scheme<ColorToColorType<T>> {
   const primaryColorType = getColorType(primary)
   // 创建基本配色方案
-  const colorsScheme = Scheme.createBaseColorScheme(primary)
+  const colorsScheme = Scheme.createBaseColorScheme(primary, options?.formula, options?.angle)
+  const customColorScheme = options?.customColorScheme
   // 合并自定义配色方案
   if (typeof customColorScheme === 'object' && !Array.isArray(customColorScheme)) {
     for (const colorsKey in customColorScheme) {
