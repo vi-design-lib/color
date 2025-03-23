75e7a591e7a94af596cf29582a34f9ca4e4ae6b7
ZhuChongLin <8210856@qq.com>
2025-03-24 03:09:09 +0800

    test(colorScheme): 更新色彩方案测试代码
    - 引入三种不同的色彩模式：triadic、adjacent、complementary
    - 循环生成并打印每种模式下的色彩方案
    - 修改示例色彩方案，使用新的主色调和辅助色
    - 优化代码结构，提高可读性和可维护性

--- __TESTS__/colorScheme.ts
+++ __TESTS__/colorScheme.ts
@@ -1,14 +1,19 @@
 import { logColorsWithLabels, Scheme } from '../src/index.js'
 
-const scheme = Scheme.createBaseColorScheme('#1376e7')
-// 打印所有颜色的 HEX 值
-logColorsWithLabels(scheme as any)
+const mode = ['triadic', 'adjacent', 'complementary']
+mode.forEach((m) => {
+  const scheme = Scheme.createBaseColorScheme('#461faa', m as any)
+  // 打印所有颜色的 HEX 值
+  logColorsWithLabels(scheme as any)
+
+  console.log('')
+})
 
 const s = {
-  main: '#1376E7',
-  aux: '#3AD9B7',
-  minor: '#6C44CF',
-  warning: '#E48A2F',
-  error: '#E42F2F',
-  neutral: '#65707C'
+  primary: '#451FA8',
+  aux: '#BA0F8A',
+  minor: '#B04319',
+  warning: '#A46525',
+  error: '#B01919',
+  neutral: '#5D5573'
 }
