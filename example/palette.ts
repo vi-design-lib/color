aaa53c9191770192b295003e7e8b8fc74ddae83f
ZhuChongLin <8210856@qq.com>
2025-09-13 23:04:00 +0800

    refactor(example): 重构调色板示例
    - 引入了新的 imports，包括 AnyColor 类型和 logColorsWithLabels 函数
    - 创建了 platte 实例并获取颜色数组
    - 将颜色数组转换为键值对对象，键为索引+1
    - 使用 logColorsWithLabels 函数输出带有标签的颜色

--- example/palette.ts
+++ example/palette.ts
@@ -1,4 +1,12 @@
-import { Palette } from '../src/index.js'
+import { type AnyColor, logColorsWithLabels, Palette } from '../src/index.js'
 
-const platte = new Palette('#3463eb', 10, { min: 0.1, max: 0.9 })
-console.log(platte.all().reverse())
+const platte = new Palette('#3463eb', 10, { min: 0.14, max: 0.86 })
+const colors = platte.all().reverse()
+
+// 将 colors 数组转换为键值对对象，键为索引+1
+const colorObject: Record<string, AnyColor> = {}
+colors.forEach((color, index) => {
+  colorObject[index + 1 + ''] = color
+})
+
+logColorsWithLabels(colorObject)
