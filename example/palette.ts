0b8491e86c73fd46cc3f2c33268fbc99207cae65
ZhuChongLin <8210856@qq.com>
2025-09-14 03:19:12 +0800

    refactor(palette): 优化颜色生成算法并简化代码
    - 移除了复杂的条件判断和冗余的代码
    - 使用统一的插值方法计算亮度，提高了代码的可读性和可维护性
    - 在示例中添加了第二个调色板实例，并优化了 CSS变量的输出格式

--- example/palette.ts
+++ example/palette.ts
@@ -2,15 +2,19 @@ import { type AnyColor, logColorsWithLabels, Palette } from '../src/index.js'
 
 const platte = new Palette('#3463eb', 10, { min: 0.14, max: 0.86 })
 const colors = platte.all().reverse()
-
 // 将 colors 数组转换为键值对对象，键为索引+1
 const colorObject: Record<string, AnyColor> = {}
 colors.forEach((color, index) => {
   colorObject[index + 1 + ''] = color
 })
-
 logColorsWithLabels(colorObject)
-
-const m3Platte = Palette.create('#3463eb', 101)
+const m3Platte = Palette.create('#9371ed', 101)
+const colorObject2: Record<string, AnyColor> = {}
+m3Platte.all().forEach((color, index) => {
+  colorObject2[index + 1 + ''] = color
+})
+logColorsWithLabels(colorObject2)
+const cssVariables1 = colors.map((color, i) => `--color-a-${i}: ${color}`)
+console.log(cssVariables1.join(';\n'))
 const cssVariables = m3Platte.all().map((color, i) => `--color-${i}: ${color}`)
 console.log(cssVariables.join(';\n'))
